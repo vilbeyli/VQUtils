c040497e0792c08a08779b9b9afc7c2e3ad46a1b
Volkan <volkanilbeyli@gmail.com>
2020-06-08 00:48:25 -0700

    Added StrUtil::UnicodeToASCII() for WCHAR[] arrays as a template fn

--- Source/utils.h
+++ Source/utils.h
@@ -46,6 +46,15 @@ namespace StrUtil
 
 	       std::string  UnicodeToASCII(const PWSTR pwstr); // https://codingtidbit.com/2020/02/09/c17-codecvt_utf8-is-deprecated/
 	inline std::wstring ASCIIToUnicode(const std::string& str) { return std::wstring(str.begin(), str.end()); }
+
+	template<unsigned STR_SIZE>
+	std::string UnicodeToASCII(const WCHAR wchars[STR_SIZE])
+	{
+		char ascii[STR_SIZE];
+		size_t numCharsConverted = 0;
+		wcstombs_s(&numCharsConverted, ascii, wchars, STR_SIZE);
+		return std::string(ascii);
+	}
 }
 
 
