1d2e19c35493258d258fecab5c2120767a720650
Volkan <volkanilbeyli@gmail.com>
2020-07-06 20:16:24 -0700

    Add stb image library + an Image wrapper to drive stb

---
+++ Source/Image.cpp
@@ -0,0 +1,47 @@
+//	VQUtils
+//	Copyright(C) 2020 - Volkan Ilbeyli
+//
+//	This program is free software : you can redistribute it and / or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.If not, see <http://www.gnu.org/licenses/>.
+//
+//	Contact: volkanilbeyli@gmail.com
+
+#include "Image.h"
+#include "Log.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../Libs/stb/stb_image.h"
+
+Image Image::LoadFromFile(const char* pFilePath, bool bHDR)
+{
+    Image img;
+    constexpr int reqComp = 0;
+    img.pData = bHDR
+        ? (void*)stbi_loadf(pFilePath, &img.x, &img.y, &img.BytesPerPixel, reqComp)
+        : (void*)stbi_load(pFilePath, &img.x, &img.y, &img.BytesPerPixel, reqComp);
+    if (img.pData == nullptr)
+    {
+        Log::Error("Error loading file: %s", pFilePath);
+    }
+    return img;
+}
+
+void Image::Destroy()
+{ 
+	if (pData) 
+	{ 
+		stbi_image_free(pData); 
+		pData = nullptr; 
+	} 
+}
+
---
+++ Source/Image.h
@@ -0,0 +1,30 @@
+//	VQUtils
+//	Copyright(C) 2020 - Volkan Ilbeyli
+//
+//	This program is free software : you can redistribute it and / or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.If not, see <http://www.gnu.org/licenses/>.
+//
+//	Contact: volkanilbeyli@gmail.com
+
+#pragma once
+
+struct Image
+{
+    static Image LoadFromFile(const char* pFilePath, bool bHDR);
+    void Destroy();  // Destroy must be called following a LoadFromFile() to prevent memory leak
+
+    union { int x; int Width; };
+    union { int y; int Height; };
+    int BytesPerPixel = 0;
+    void* pData = nullptr;
+};
