d1815dd6834fb991486f85b429556704e5e394a6
Volkan <volkanilbeyli@gmail.com>
2020-07-22 21:42:50 -0700

    move string parsers into StrUtil

--- Source/utils.h
+++ Source/utils.h
@@ -19,6 +19,7 @@
 #pragma once
 
 #include <string>
+#include <sstream>
 #include <vector>
 #include <locale>
 
@@ -33,6 +34,11 @@
 
 namespace StrUtil
 {
+		
+	inline bool  ParseBool (const std::string& s) { bool b; std::istringstream(s) >> std::boolalpha >> b; return b; }
+	inline int   ParseInt  (const std::string& s) { return std::atoi(s.c_str()); }
+	inline float ParseFloat(const std::string& s) { return static_cast<float>(std::atof(s.c_str())); }
+
 	std::vector<std::string> split(const char* s, char c = ' ');
 	std::vector<std::string> split(const std::string& s, char c = ' ');
 	std::vector<std::string> split(const std::string& s, const std::vector<char>& delimiters);
